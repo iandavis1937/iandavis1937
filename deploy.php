5d6fb7121decd513e5f56c8ec934f9ef60dd0a1c
ianda <iandavis1937@gmail.com>
2026-03-10 21:45:18 -0600

    Web hook setup #2

--- deploy.php
+++ deploy.php
@@ -6,5 +6,5 @@ if (!hash_equals($sig, $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '')) {
     http_response_code(403); exit;
 }
 
-shell_exec('cd /home/iandavis1937/public_html && git pull origin main 2>&1');
+shell_exec('cd /home/iandavis1937/public_html && git pull origin master 2>&1');
 echo "Deployed!";
