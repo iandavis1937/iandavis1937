4ed9ffe1caa1ddef19caa5b3072e962fa973e4dc
ianda <iandavis1937@gmail.com>
2026-03-10 21:43:42 -0600

    Webhook setup #1
    Initial attempt

---
+++ deploy.php
@@ -0,0 +1,10 @@
+<?php
+$secret = 'among-hedgehog-garden';
+$sig = 'sha256=' . hash_hmac('sha256', file_get_contents('php://input'), $secret);
+
+if (!hash_equals($sig, $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '')) {
+    http_response_code(403); exit;
+}
+
+shell_exec('cd /home/iandavis1937/public_html && git pull origin main 2>&1');
+echo "Deployed!";
